12e95e38e099625f6242dd09eb54d94fa45b1df0
carolin-violet <2976008778@qq.com>
2022-05-26 09:18:00 +0800

    v-1.0

--- src/views/pictures/index.vue
+++ src/views/pictures/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>上传各种照片</div>
+  <div class="app-container">
+    隐藏的，可以查看更多图片并添加更多图片
+  </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+  data() {
+    return {
+      markeId: null
+    }
+  },
+
+  created() {
+    console.log(this.$route.query.mark_id)
+    this.markeId = this.$route.query.mark_id
+  }
 }
 </script>
 
