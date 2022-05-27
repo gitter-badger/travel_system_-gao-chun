3daa397058c375b0fa42ff08d499570fe18f34da
carolin-violet <2976008778@qq.com>
2022-05-27 13:18:01 +0800

    v-1.0

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <h1>欢迎使用高淳旅游服务管理系统</h1>
+    <video width="1280" height="720" controls="controls" autoplay="autoplay">
+      <source
+        src="https://outin-ccdd316fd0ec11ec8e0700163e06123c.oss-cn-shanghai.aliyuncs.com/656fb783b911432a85f99c4a20b3b51c/d5a4b5627d0a8eeb508d2e7a929b7ef3-sd.mp4?Expires=1653631106&OSSAccessKeyId=LTAIrkwb21KyGjJl&Signature=hc3jJV3vFzavIQRpvNJxOOa2LLE%3D"
+        type="video/mp4"
+      />
+    </video>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+
 
 export default {
   name: 'Dashboard',
@@ -18,13 +25,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+.dashboard-container{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  h1{
+    text-align: center;
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
+  video{
+    align-self: center;
   }
 }
+
 </style>
